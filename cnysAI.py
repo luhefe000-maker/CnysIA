b7273f33410c4802d23af4156d39be38009cea38
gbarbosa0811 <gb08112007@gmail.com>
2026-03-18 00:52:12 -0300

    teste

--- cnysAI.py
+++ cnysAI.py
@@ -1,1 +1,14 @@
 print("Hello World!")
+
+import os
+import PyPDF2
+
+
+merge = PyPDF2.PdfMerger()   
+lista_arquivos = os.listdir("arquivos")   
+lista_arquivos.sort()
+
+for arquivo in lista_arquivos:
+    if ".pdf" in arquivo:
+        merge.append(f"arquivos/{arquivo}")
+        merge.write("pdf_organizado.pdf")
